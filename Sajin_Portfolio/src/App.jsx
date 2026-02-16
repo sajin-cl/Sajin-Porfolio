cf0b61839db6deb2834509f64f8b7c4d45a7978d
Sajincl-03 <sajincl222@gmail.com>
2026-02-17 04:46:12 +0530

    lazy loading updated

--- Sajin_Portfolio/src/App.jsx
+++ Sajin_Portfolio/src/App.jsx
@@ -1,24 +1,37 @@
+import { lazy, Suspense } from 'react'
+
 import Header from "./components/Header"
 import Hero from "./components/Hero"
-import About from "./components/About"
-import Skills from "./components/Skills"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
 import Footer from "./components/Footer"
-import Leetcode from "./components/Leetcode"
+
+const About = lazy(() => import("./components/About"));
+const Skills = lazy(() => import("./components/Skills"));
+const Leetcode = lazy(() => import("./components/Leetcode"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
+
 
 function App() {
-  d
+
   return (
     <div className="min-h-screen flex flex-col scroll-smooth">
       <Header />
       <main className="pt-16" >
         <Hero />
-        <About />
-        <Skills />
-        <Leetcode />
-        <Projects />
-        <Contact />
+        <Suspense fallback={
+          <div className="space-y-4 p-6 animate-pulse">
+            <div className="h-6 bg-gray-700 rounded w-1/3"></div>
+            <div className="h-4 bg-gray-700 rounded w-full"></div>
+            <div className="h-4 bg-gray-700 rounded w-5/6"></div>
+          </div>
+        }>
+          <About />
+          <Skills />
+          <Leetcode />
+          <Projects />
+          <Contact />
+        </Suspense>
+
       </main>
       <Footer />
     </div>
