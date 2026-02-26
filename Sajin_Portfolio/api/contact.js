4da908a7c411b7cb572ecae7f941bcfe856cf95f
Sajincl-03 <sajincl222@gmail.com>
2026-02-27 01:02:56 +0530

    contact us setup

---
+++ Sajin_Portfolio/api/contact.js
@@ -0,0 +1,39 @@
+import nodemailer from 'nodemailer';
+
+export default async function contactUs(req, res) {
+  try {
+
+    if (req.method !== "POST") return res.status(405).json({ message: "Method not allowed" });
+
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) return res.status(400).json({ message: 'All Fields are required' });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+
+    const mailOptions = {
+      from: `"PORTFOLIO REPLY" <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_USER,
+      subject: `portfolio response from ${name} `,
+      text: `
+      Name: ${name}
+      Email: ${email}
+      Message: ${message}
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json({ message: "Message sent successfully!" });
+  }
+  catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Message not sent!" });
+  }
+
+}
--- Sajin_Portfolio/src/components/Contact.jsx
+++ Sajin_Portfolio/src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { FiAlertTriangle } from "react-icons/fi";
 import Typewriter from "typewriter-effect";
 import { motion } from 'framer-motion';
@@ -6,21 +6,56 @@ import { motion } from 'framer-motion';
 
 const Contact = () => {
 
-  const [alert, setAlert] = useState('');
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(null);
 
-  const alertRef = useRef(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
 
-  const handleClick = () => {
-    setAlert("Sorry for your inconvenience. Currently it's not working. Contact me on Email or +91 8056825814");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-    setTimeout(() => {
-      alertRef.current?.scrollIntoView({ behavior: "smooth", block: "center" });
-    }, 100);
-
-    setTimeout(() => {
-      setAlert("")
-    }, 4000);
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
   };
+
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setStatus('success');
+        setMessage("Message sent successfully!");
+        setFormData({ name: "", email: "", message: "" });
+        setTimeout(() => setMessage(""), 3000);
+      } else {
+        setStatus('error');
+        setMessage(data.message);
+        setTimeout(() => setMessage(""), 3000);
+      }
+
+    } catch (err) {
+      setStatus('error');
+      setMessage("Something went wrong");
+      setTimeout(() => setMessage(""), 3000);
+    }
+  }
+
 
   return (
     <section id="contact" className="min-h-screen bg-stone-950 flex items-center justify-center p-4 mx-auto">
@@ -44,13 +79,19 @@ const Contact = () => {
         </div>
 
         <div className="border border-lime-300 shadow-xl px-3 w-full p-5 rounded-2xl">
-          <form className="flex flex-col gap-3 p-3">
+          <form
+            onSubmit={submitForm}
+            className="flex flex-col gap-3 p-3"
+          >
             <div>
               <label className="block text-sm  text-lime-200 mb-1">
                 Name :
               </label>
               <input
                 type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
                 placeholder="Your name"
                 className="w-full border  text-gray-300 text-sm py-4 border-gray-800 rounded-lg px-4  focus:outline-none focus:ring-2 focus:ring-lime-400"
               />
@@ -62,6 +103,9 @@ const Contact = () => {
               </label>
               <input
                 type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
                 placeholder="your@gmail.com"
                 className="w-full border  text-gray-300 py-4 text-sm border-gray-800 rounded-lg px-4  focus:outline-none focus:ring-2 focus:ring-lime-400"
               />
@@ -73,6 +117,9 @@ const Contact = () => {
               </label>
               <textarea
                 rows="4"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
                 placeholder="Write your message..."
                 className="w-full border border-gray-800 text-gray-300 text-sm rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-lime-400"
               ></textarea>
@@ -80,21 +127,23 @@ const Contact = () => {
 
             <button
               type="submit"
-              className="w-full bg-lime-400 text-white py-2 rounded-full hover:bg-lime-600 transition duration-200 font-medium cursor-no-drop"
-              onClick={handleClick}>
+              className="w-full bg-lime-400 text-white py-2 rounded-full hover:bg-lime-600 transition duration-200 font-medium"
+            >
               Send Message
             </button>
           </form>
+          {message && (
+            <div
+              className={`text-xs mt-3 text-center leading-relaxed tracking-wide flex items-start justify-center gap-1 max-w-sm mx-auto ${status === "success" ? "text-green-500" : "text-red-600"
+                }`}
+            >
+              <FiAlertTriangle size={15} className="shrink-0 mt-0.5" />
+              <span>{message}</span>
+            </div>
+          )}
         </div>
-        {alert && (
-          <div
-            ref={alertRef}
-            className="text-red-600 text-xs mt-3 text-center  leading-relaxed tracking-wide flex items-start justify-center gap-1 max-w-sm mx-auto"
-          >
-            <FiAlertTriangle size={15} className="shrink-0 mt-0.5" />
-            <span >{alert}</span>
-          </div>
-        )}
+
+
 
       </div>
     </section>
