bbcf2835c8781bab00b57caca1a14b731fa67224
Sajincl-03 <sajincl222@gmail.com>
2026-02-27 13:04:45 +0530

    scrollbar added to cartificate page

--- Sajin_Portfolio/src/components/Certifications.jsx
+++ Sajin_Portfolio/src/components/Certifications.jsx
@@ -1,13 +1,31 @@
-import { certificationsData } from '../data/data'
+import { certificationsData } from '../data/data';
 import { motion } from "framer-motion";
+import { MdKeyboardDoubleArrowRight, MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { useRef } from "react";
 
 const Certifications = () => {
+  const carouselRef = useRef(null);
+
+  // Scroll by one card width
+  const scroll = (direction) => {
+
+    const card = carouselRef.current.querySelector(".carousel-card");
+
+    const scrollAmount = card.clientWidth + 16;
+
+    if (direction === "left") {
+      carouselRef.current.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    } else {
+      carouselRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    }
+  };
+
   return (
     <section
       id="certifications"
       className="flex min-h-screen justify-between items-center bg-stone-950"
     >
-      <div className="w-full py-10 bg-stone-950 flex flex-col items-center gap-8">
+      <div className="w-full py-10 flex flex-col items-center gap-8 relative">
 
         {/* Title */}
         <div className="text-center">
@@ -21,61 +39,69 @@ const Certifications = () => {
           </motion.h2>
         </div>
 
-        {/* Certificate Container*/}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-6xl px-4 ">
-          {certificationsData.map((cert, index) => (
-            <div
-              key={index}
-              className={`bg-stone-950 rounded-2xl overflow-hidden flex flex-col outline outline-gray-900
-              hover:outline-lime-900 hover:shadow-[0_15px_25px_rgba(0,255,0,0.3)] px-2 py-1 `}
-            >
+        {/* Carousel */}
+        <div className="relative w-full max-w-6xl flex items-center">
 
-              {/* Certificate Image */}
-              <img
-                src={cert?.image}
-                alt={cert?.name}
-                className="w-full h-48 object-cover "
-              />
+          {/* Left Arrow  */}
+          <MdKeyboardDoubleArrowLeft
+            size={40}
+            className="absolute left-0 z-10 cursor-pointer text-lime-300"
+            onClick={() => scroll("left")}
+          />
 
-              {/* Certificate Info */}
-              <div className="p-6 flex flex-col grow">
-                <h3 className="text-xl lg:text-2xl font-semibold text-white mb-2">
-                  {cert?.name}
-                </h3>
+          {/* Scrollable container */}
+          <div
+            ref={carouselRef}
+            className="flex overflow-x-auto scroll-smooth gap-4 w-full px-10 py-10 touch-pan-x carousel-scrollbar-hide"
+          >
+            {certificationsData.map((cert, index) => (
+              <div
+                key={index}
+                className="carousel-card shrink-0 w-full md:w-1/2 lg:w-1/3 bg-stone-950 rounded-2xl overflow-hidden flex flex-col outline outline-gray-900 hover:outline-lime-900 hover:shadow-[0_15px_25px_rgba(0,255,0,0.3)]"
+              >
+                {/* Certificate Image */}
+                <img
+                  src={cert?.image}
+                  alt={cert?.name}
+                  className="w-full h-48 object-cover"
+                />
 
-                <p className="text-lime-400 mb-2 text-sm">
-                  {cert?.issuer}
-                </p>
-
-                <ul className="text-gray-300 mb-4 flex-1 leading-relaxed text-sm list-disc list-inside space-y-1">
-                  {cert?.description &&
-                    cert.description
-                      .split(".")
-                      .filter(item => item.trim() !== "")
-                      .map((point, i) => (
-                        <li key={i}>{point.trim()}</li>
-                      ))}
-                </ul>
-
-                {/* Button */}
-                <a
-                  href={cert?.credential}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={`px-4 py-2 text-md text-center w-full rounded-full transition-all duration-300 
-                   ${cert?.credential !== "#"
-                      ? "outline outline-lime-400 bg-lime-600 hover:bg-lime-500 hover:shadow-[0_0_15px_#84ff00] cursor-pointer text-white"
-                      : "outline outline-gray-700 text-gray-500 cursor-not-allowed"}
-  `}
-                >
-                  {cert?.credential !== "#" ? "View Credential" : "Not Credential"}
-                </a>
+                {/* Certificate Info */}
+                <div className="p-6 flex flex-col grow">
+                  <h3 className="text-xl lg:text-2xl font-semibold text-white mb-2">
+                    {cert?.name}
+                  </h3>
+                  <p className="text-lime-400 mb-2 text-sm">{cert?.issuer}</p>
+                  <ul className="text-gray-300 mb-4 flex-1 leading-relaxed text-sm list-disc list-inside space-y-1">
+                    {cert?.description &&
+                      cert.description
+                        .split(".")
+                        .filter((item) => item.trim() !== "")
+                        .map((point, i) => <li key={i}>{point.trim()}</li>)}
+                  </ul>
+                  <a
+                    href={cert?.credential}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={`px-4 py-2 text-md text-center w-full rounded-full transition-all duration-300 ${cert?.credential !== "#"
+                        ? "outline outline-lime-400 bg-lime-600 hover:bg-lime-500 hover:shadow-[0_0_15px_#84ff00] cursor-pointer text-white"
+                        : "outline outline-gray-700 text-gray-500 cursor-not-allowed"
+                      }`}
+                  >
+                    {cert?.credential !== "#" ? "View Credential" : "Not Credential"}
+                  </a>
+                </div>
               </div>
+            ))}
+          </div>
 
-            </div>
-          ))}
+          {/* Right Arrow  */}
+          <MdKeyboardDoubleArrowRight
+            size={40}
+            className="absolute right-0 md:right-[-6px] lg:right-[-30px] z-10 cursor-pointer text-lime-300"
+            onClick={() => scroll("right")}
+          />
         </div>
-
       </div>
     </section>
   );
