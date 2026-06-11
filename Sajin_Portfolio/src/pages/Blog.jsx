461881b4f970b99fd383ffac4be5381fd0ef40a3
sajin-cl <sajincl222@gmail.com>
2026-06-11 17:38:01 +0530

    small change

--- Sajin_Portfolio/src/pages/Blog.jsx
+++ Sajin_Portfolio/src/pages/Blog.jsx
@@ -2,7 +2,7 @@ export default function Blog() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-stone-950 text-white px-6 py-16">
       <div className="max-w-3xl text-center">
-        <p className="text-sm text-lime-300 uppercase tracking-[0.3em] mb-4 animate-pulse">Currently No Blog are available </p>
+        <p className="text-sm text-lime-300 uppercase tracking-[0.3em] mb-4 animate-pulse">Currently No Blogs are available </p>
         
       </div>
     </div>
